99ee9b0308453fecaebc25a4cebe9076ceb0f7b5
Roney Freitas <roneyfreitas.rf@gmail.com>
2020-12-14 23:09:38 -0300

    Commit inicial

---
+++ config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteTitle: 'Portfolio Roney', // <title>
+  manifestName: 'Resume',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-resume/`, // This path is subpath of your hosting https://domain/portfolio
+  firstName: 'Roney',
+  lastName: 'Freitas',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/roneydev01',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/roney-freitas-99b264114/',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/onlyanubhav',
+    },
+    {
+      icon: 'fa-facebook-f',
+      name: 'Facebook',
+      url: 'https://facebook.com/theanubhav',
+    },
+  ],
+  email: 'roneyfreitas.rf@gmail.com',
+  phone: '85-998199567',
+  address: 'Fortaleza-CE, Brasil',
+};
