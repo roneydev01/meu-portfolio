5b6f9b8a2b7d6db04973a1932b90e2167196189e
Roney Freitas <roneyfreitas.rf@gmail.com>
2020-12-15 23:19:59 -0300

    Criado Componente Resume

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,42 +4,25 @@ import Layout from '../components/Layout';
 
 // import { Link } from 'gatsby';
 import Sidebar from '../components/Sidebar';
+import Resume from '../components/sections/Resume';
 import config from '../../config';
 const IndexPage = () => (
   <Layout>
     <Sidebar />
     <div className="container-fluid p-0">
-      <section
-        className="resume-section p-3 p-lg-5 d-flex align-items-center"
-        id="about"
+      <Resume
+        firstName={config.firstName}
+        lastName={config.lastName}
+        address={config.address}
+        phone={config.phone}
+        email={config.email}
+        socialLinks={config.socialLinks}
       >
-        <div className="w-100">
-          <h1 className="mb-0">
-            {config.firstName}
-            <span className="text-primary">{config.lastName}</span>
-          </h1>
-          <div className="subheading mb-5">
-            {config.address} · {config.phone} ·
-            <a href={`mailto:${config.email}`}>{config.email}</a>
-          </div>
-          <p className="lead mb-5">
-            I am experienced in leveraging agile frameworks to provide a robust
-            synopsis for high level overviews. Iterative approaches to corporate
-            strategy foster collaborative thinking to further the overall value
-            proposition.
-          </p>
-          <div className="social-icons">
-            {config.socialLinks.map(social => {
-              const { icon, url } = social;
-              return (
-                <a key={url} href={url}>
-                  <i className={`fab ${icon}`}></i>
-                </a>
-              );
-            })}
-          </div>
-        </div>
-      </section>
+        Tenho experiência em aproveitar estruturas ágeis para fornecer uma
+        sinopse para visões gerais de alto nível. Abordagens iterativas para empresas
+        com estratégia, promover o pensamento colaborativo para promover o valor geral
+        proposição.
+      </Resume>
 
       <hr className="m-0" />
 
