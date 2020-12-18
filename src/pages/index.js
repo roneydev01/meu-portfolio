9492bd796ad2cc79263a5bbb2efe34c40d9ad3a8
Roney Freitas <roneyfreitas.rf@gmail.com>
2020-12-17 22:49:50 -0300

    Criado componente Education

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import Layout from '../components/Layout';
 import Sidebar from '../components/Sidebar';
 import Resume from '../components/sections/Resume';
 import Experience from '../components/sections/Experience';
+import Education from '../components/sections/Education';
 import config from '../../config';
 
 const skillsData = [
@@ -47,37 +48,7 @@ const IndexPage = () => (
 
       <hr className="m-0" />
 
-      <section
-        className="resume-section p-3 p-lg-5 d-flex align-items-center"
-        id="education"
-      >
-        <div className="w-100">
-          <h2 className="mb-5">Formação</h2>
-
-          <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-            <div className="resume-content">
-              <h3 className="mb-0">University of Colorado Boulder</h3>
-              <div className="subheading mb-3">Bachelor of Science</div>
-              <div>Computer Science - Web Development Track</div>
-              <p>GPA: 3.23</p>
-            </div>
-            <div className="resume-date text-md-right">
-              <span className="text-primary">August 2006 - May 2010</span>
-            </div>
-          </div>
-
-          <div className="resume-item d-flex flex-column flex-md-row justify-content-between">
-            <div className="resume-content">
-              <h3 className="mb-0">James Buchanan High School</h3>
-              <div className="subheading mb-3">Technology Magnet Program</div>
-              <p>GPA: 3.56</p>
-            </div>
-            <div className="resume-date text-md-right">
-              <span className="text-primary">August 2002 - May 2006</span>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Education />
 
       <hr className="m-0" />
 
